f8f76a86d13065c157384af4f91e0bbae4601558
peppecrouch <giuseppedurante6@gmail.com>
2026-02-28 15:19:03 +0100

    Added /duel command

--- src/main/java/me/peppecrouch/proudDuels/commands/DuelCommand.java
+++ src/main/java/me/peppecrouch/proudDuels/commands/DuelCommand.java
@@ -1,5 +1,6 @@
 package me.peppecrouch.proudDuels.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,13 +12,24 @@ public class DuelCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+
+        Player target;
+
         if (sender instanceof Player player) {
             if (command.getName().equalsIgnoreCase("duel")) {
-                if (args.length == 0) {}
+                if (args.length == 0) {
+                    sender.sendMessage(ChatColor.RED + "/duel <player>");
+                    return true;
+                }
+                if (args.length == 1) {
+                    target = Bukkit.getPlayer(args[0]);
+                    if (target.isOnline()) {
+                        target.sendMessage(ChatColor.RED + "Player " + ChatColor.YELLOW + args[0] + " is online.");
+                        return true;
+                    }
+                }
             }
         }
-
-
         return false;
     }
 }
